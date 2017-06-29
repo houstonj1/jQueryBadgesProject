fd2e2c9c3cd03dd65e322fec15357121755618c8
houstonj1 <houstonj1@wit.edu>
2017-06-29 15:13:46 -0400

    5

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
-
+    $.ajax({
+        url: 'https://www.codeschool.com/users/houstonj1',
+        dataType: 'jsonp',
+        success: function() {
+            // success stuff
+        }
+    });
   // your code will go here
 
 });
