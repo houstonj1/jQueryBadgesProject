3f78fdb624d7d6777afe976ff0a156caf635e408
houstonj1 <houstonj1@wit.edu>
2017-07-06 12:55:44 -0400

    6

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,8 +3,11 @@ $(function() {
         url: 'https://www.codeschool.com/users/houstonj1.json',
         dataType: 'jsonp',
         success: function(response) {
-            
             // success stuff
+            for(badge in response.courses.completed) {
+                var item = $('<div class="course"></div>');
+                $('#badges').append(item);
+            }
         }
     });
   // your code will go here
